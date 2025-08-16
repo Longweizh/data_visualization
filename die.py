32d30b0b8eed3c24850d349c1bac003949e51302
Longwei Zhang <longwei2@illinois.edu>
2025-08-16 18:57:08 -0500

    Random walk and Rolling Dice

---
+++ die.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# -*- coding=utf-8 -*-
+
+#===================================
+# @Filename: die.py
+# @Author: Longwei Zhang
+# @Create Time: 8/16/25 18:13
+# @Email: longwei2@illinois.edu
+#===================================
---
+++ die_visual.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# -*- coding=utf-8 -*-
+
+#===================================
+# @Filename: die_visual.py
+# @Author: Longwei Zhang
+# @Create Time: 8/16/25 18:20
+# @Email: longwei2@illinois.edu
+#===================================
